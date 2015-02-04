a3bb5ac586401c5d8ff37f32c56e4cd8084a4988
Claude Phan <claudephan.pi@gmail.com>
2015-02-04 12:11:26 -0800

    OS project

---
+++ OS_CS431/File.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_CS431
+{
+    class File
+    {
+        public string FileName;
+        public string FileExt;
+        public string FileText;
+
+        public void setFileName(string FN)
+        {
+            FileName = FN;
+        }
+        public string getFileName()
+        {
+            return FileName;
+        }
+
+        public void setFileExt(string Ext)
+        {
+            FileName = Ext;
+        }
+        public string getFileExt()
+        {
+            return FileExt;
+        }
+
+        public void setFileText(string txt)
+        {
+            FileName = txt;
+        }
+        public string getFileText()
+        {
+            return FileText;
+        }
+
+    }
+}
---
+++ OS_CS431/Kernel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace OS_CS431
+{
+    
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            File F1 = new File();
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            F1.setFileName(input);
+            Console.Write("File name is " + F1.getFileName());
+            //Console.WriteLine(input);
+
+            
+
+
+        }
+    }
+}
